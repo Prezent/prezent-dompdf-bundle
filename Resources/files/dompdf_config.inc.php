931754d92b4523fc024b3cf3a6a56ccb2e99d9ba
Robert-Jan Bijl <rober-jan@prezent.nl>
2015-11-11 10:02:53 +0100

    update default config file

--- Resources/files/dompdf_config.inc.php
+++ Resources/files/dompdf_config.inc.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 // these settings are required for the framwork integration, but change them as you see fit
-define("DOMPDF_FONT_DIR", "../var/cache/");
-define("DOMPDF_FONT_CACHE", "../var/cache/");
-define("DOMPDF_ENABLE_AUTOLOAD", false);
+define('DOMPDF_FONT_DIR', __DIR__ . '/../../app/cache/');
+define('DOMPDF_FONT_CACHE', __DIR__ . '/../../app/cache/');
+define('DOMPDF_ENABLE_AUTOLOAD', false);
 
 // define your own custom setting below
 //define("DOMPDF_DPI", 300);
@@ -12,4 +12,4 @@ define("DOMPDF_ENABLE_AUTOLOAD", false);
 //define("DOMPDF_ENABLE_CSS_FLOAT", true);
 
 // include the DOMPDF config file
-include('../vendor/dompdf/dompdf/dompdf_config.inc.php');
+include  __DIR__ . '/../../vendor/dompdf/dompdf/dompdf_config.inc.php';
