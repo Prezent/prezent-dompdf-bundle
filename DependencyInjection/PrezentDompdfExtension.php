50f754add3c4db8839785cea99012c31e2c5186a
Robert-Jan Bijl <r.j.bijl@gmail.com>
2021-02-12 11:08:10 +0100

    no more curly braces array access...

--- DependencyInjection/PrezentDompdfExtension.php
+++ DependencyInjection/PrezentDompdfExtension.php
@@ -28,7 +28,7 @@ class PrezentDompdfExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
-        if (isset($config{'config_location'})) {
+        if (isset($config['config_location'])) {
             $container->setParameter('prezent_dompdf.config_location', $config['config_location']);
         }
 
